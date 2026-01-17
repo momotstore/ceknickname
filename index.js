455d217c2e837847f3357d6a3cbcde190b24b783
momotstore <l.utfi.inzaghi89@gmail.com>
2026-01-18 02:36:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,10 @@ app.use(cors({
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public'), {
+  index: false // ⬅️ INI KUNCINYA
+}));
 
 app.use('/api', router);
 
