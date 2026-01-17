747e3469dd57b947fb1842fa56fb93db4f8815df
momotstore <l.utfi.inzaghi89@gmail.com>
2026-01-18 02:08:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -73,11 +73,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', router);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', '404.html'));
 });
-app.get('/dokumentasi', (req, res) => {
-   res.sendFile(path.join(__dirname, 'public', 'dokumentasi.html'));
-});
+// app.get('/dokumentasi', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'public', 'dokumentasi.html'));
+// });
 
 app.get('/endpoint', (req, res) => {
    const newDataGame = dataGame.map((item) => {
@@ -97,8 +97,10 @@ app.get('/endpoint', (req, res) => {
    });
 });
 
-app.get('/*', (req, res) => {
-   res.status(404).json({ error: 'Error' });
+app.use((req, res) => {
+  res.status(404).sendFile(
+    path.join(__dirname, 'public', '404.html')
+  );
 });
 
 app.listen(port, () => {
