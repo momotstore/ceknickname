34aeb1444df69b616f14ed4518432bfc8fa29c25
momotstore <l.utfi.inzaghi89@gmail.com>
2026-01-19 01:24:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use('/test', express.static('public'));
 app.use('/api', router);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', '404.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('/dokumentasi', (req, res) => {
    res.sendFile(path.join(__dirname, 'public', 'dokumentasi.html'));
