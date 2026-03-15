792f303ddd36213093bfc936254fc47398232f97
momotstore <l.utfi.inzaghi89@gmail.com>
2026-03-15 19:08:25 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getZoneController, idCheckController } = require('../controllers');
 
+const wwmGame = require('./wwmGames'); 
+
 const routes = [
    {
       method: 'GET',
@@ -14,6 +16,28 @@ const routes = [
       controller: getZoneController,
    },
 ];
+
+router.get('/api/wwm/:id', async (req, res) => {
+    try {
+        const userId = req.params.id; 
+
+        const result = await wwmGame(userId);
+
+        if (result.status) {
+            return res.status(200).json(result);
+        } 
+        
+        return res.status(404).json(result);
+
+    } catch (error) {
+        console.error("Error pada route /api/check-role:", error);
+        return res.status(500).json({
+            status: false,
+            message: "Terjadi kesalahan pada server"
+        });
+    }
+});
+
 
 // Menetapkan rute menggunakan objek
 routes.forEach((route) => {
